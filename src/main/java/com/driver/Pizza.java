52f673366c9743fbc11f415bbebc3b4bb3f8eb36
Sachin Biradar <84908560+Bsachin950@users.noreply.github.com>
2024-01-20 14:04:36 +0530

    Pizza bill generating

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,57 @@
 package com.driver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    private Map<String, Integer> addedComponents;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-        // your code goes here
+        this.price = isVeg ? 300 : 400;
+        this.addedComponents = new HashMap<>();
+        this.bill = "";
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese() {
+        if (!addedComponents.containsKey("Extra Cheese")) {
+            this.price += 80;
+            addedComponents.put("Extra Cheese", 80);
+        }
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings() {
+        if (!addedComponents.containsKey("Extra Toppings")) {
+            this.price += (isVeg ? 70 : 120);
+            addedComponents.put("Extra Toppings", isVeg ? 70 : 120);
+        }
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway() {
+        if (!addedComponents.containsKey("Paper Bag")) {
+            this.price += 20;
+            addedComponents.put("Paper Bag", 20);
+        }
     }
 
-    public String getBill(){
-        // your code goes here
+    public String getBill() {
+        StringBuilder billBuilder = new StringBuilder();
+        billBuilder.append("Base Price Of The Pizza: ").append(this.price).append("\n");
+
+        for (Map.Entry<String, Integer> entry : addedComponents.entrySet()) {
+            billBuilder.append(entry.getKey()).append(" Added: ").append(entry.getValue()).append("\n");
+        }
+
+        billBuilder.append("Total Price: ").append(getPrice()).append("\n");
+        this.bill = billBuilder.toString();
         return this.bill;
     }
 }
